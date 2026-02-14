df913f72635863b1f3848b6ffd67d6831ba5cf88
Rudraprasad2302 <wwwrudraprasad2302@gmail.com>
2026-02-14 21:19:29 +0530

    inverted total half pyramid

--- Day-5/Patterns.java
+++ Day-5/Patterns.java
@@ -13,8 +13,24 @@ public class Patterns {
         }
 
     }
+
+
+    public static void inverted_totated_half_pyramid(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                //print spaces
+                System.out.print(" ");
+            }
+            //print stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        holRect(10, 10);
+        //holRect(10, 10);
+        inverted_totated_half_pyramid(10);
         
     }
 }
