99bbfa215a875f9aa1a966f54e1722db81a47be2
Rudraprasad2302 <wwwrudraprasad2302@gmail.com>
2026-02-15 18:20:45 +0530

    Butterfly Pattern

--- Day-5/Patterns.java
+++ Day-5/Patterns.java
@@ -61,12 +61,49 @@ public class Patterns {
             System.out.println();
         }
     }
+
+    public static void butterfly_pattern(int n){
+        //1st half
+        for(int i=1; i<=n; i++){
+            //stars- i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            //spaces 2*(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            //stars- i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //2nd half
+        for(int i=n; i>=1; i--){
+            //stars- i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            //spaces 2*(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            //stars- i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
  
     public static void main(String[] args) {
         //holRect(10, 10);
         //inverted_totated_half_pyramid(10);
         //inverted_half_pyramid_withNumbers(6);
         //floyds_triangle(7);
-        zero_one_triangle(5);
+        //zero_one_triangle(5);
+        butterfly_pattern(5);
     }
 }
