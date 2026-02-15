a08a9910ab34c3d5acd66fd0c0385d27d0640792
Rudraprasad2302 <wwwrudraprasad2302@gmail.com>
2026-02-15 17:24:24 +0530

    inverted half pyramid with number

--- Day-5/Patterns.java
+++ Day-5/Patterns.java
@@ -28,9 +28,20 @@ public class Patterns {
             System.out.println();
         }
     }
+
+    public static void inverted_half_pyramid_withNumbers(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+ 
     public static void main(String[] args) {
         //holRect(10, 10);
-        inverted_totated_half_pyramid(10);
+        //inverted_totated_half_pyramid(10);
+        inverted_half_pyramid_withNumbers(6);
         
     }
 }
