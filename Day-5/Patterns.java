12f3b07930a0844056585257be868f34978ec21a
Rudraprasad2302 <wwwrudraprasad2302@gmail.com>
2026-02-15 17:52:28 +0530

    Zero One Triangle

--- Day-5/Patterns.java
+++ Day-5/Patterns.java
@@ -47,11 +47,26 @@ public class Patterns {
             System.out.println();
         }
     }
+
+    public static void zero_one_triangle(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j) % 2 == 0){
+                    System.out.print("1"+" ");
+                }
+                else{
+                    System.out.print("0"+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
  
     public static void main(String[] args) {
         //holRect(10, 10);
         //inverted_totated_half_pyramid(10);
         //inverted_half_pyramid_withNumbers(6);
-        floyds_triangle(7);
+        //floyds_triangle(7);
+        zero_one_triangle(5);
     }
 }
