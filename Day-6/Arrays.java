7a7a721d0c8f650d4b7f84d380c1ca6800cebf23
Rudraprasad2302 <wwwrudraprasad2302@gmail.com>
2026-02-22 09:54:30 +0530

    Largest and smallest

--- Day-6/Arrays.java
+++ Day-6/Arrays.java
@@ -9,16 +9,38 @@ public class Arrays {
 
         return -1;
     }
-    public static void main(String[] args) {
-        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
-        int key =20;
 
-        int index = linearSearch(numbers, key);
-        if(index == -1){
-            System.out.println("Key not found");
+    public static int largest_smallest_number(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+
+        for(int i=0; i<numbers.length; i++){
+            if(largest< numbers[i]){
+                largest=numbers[i];
+            }
+
+            if(smallest> numbers[i]){
+                smallest=numbers[i];
+            }
         }
-        else{
-            System.out.println("Key is at index "+ index);
-        }
+        System.out.println("smallest value is "+ smallest);
+        return largest;
+    }
+    public static void main(String[] args) {
+        //for linear searrch
+        // int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        // int key =20;
+
+        // int index = linearSearch(numbers, key);
+        // if(index == -1){
+        //     System.out.println("Key not found");
+        // }
+        // else{
+        //     System.out.println("Key is at index "+ index);
+        // }
+
+        //largest in array
+        int numbers[] = {5, 7, 11, 8, 45, 70, 76, 58, 48, 44};
+        System.out.println("Largest value is "+ largest_smallest_number(numbers));
     }
 }
