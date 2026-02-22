d56d5d50a9d9ea010dc33bbb0b0d6df871a8b2fe
Rudraprasad2302 <wwwrudraprasad2302@gmail.com>
2026-02-22 15:50:58 +0530

    Print subarrays

--- Day-6/Arrays.java
+++ Day-6/Arrays.java
@@ -73,6 +73,23 @@ public class Arrays {
         }
         System.out.println("Total pairs: " +tp);
     }
+
+    public static void printSubArrays(int numbers[]){
+        int ts=0;
+        for(int i=0; i<numbers.length; i++){
+            int start = i;
+            for(int j=i; j<numbers.length; j++){
+                int end = j;
+                for(int k=start; k<=end; k++){
+                    System.out.print(numbers[k]+" ");
+                    ts++;
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("Total subarrays:"+ts);
+    }
     public static void main(String[] args) {
         
          int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
@@ -101,6 +118,9 @@ public class Arrays {
         // System.out.println();
 
         //Print Array in pairs
-        printPairs(numbers);
+        //printPairs(numbers);
+
+        //print sub arrays
+        printSubArrays(numbers);
     }
 }
