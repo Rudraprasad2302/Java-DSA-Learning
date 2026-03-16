86f7ebb78c0544b597ce0bb69205745ccca9943a
Rudraprasad2302 <wwwrudraprasad2302@gmail.com>
2026-03-16 21:36:15 +0530

    Bubble sort Swap updated

--- Day-7/Sorting.java
+++ Day-7/Sorting.java
@@ -1,13 +1,17 @@
 public class Sorting {
     public static void bubbleSort(int arr[]){
         for(int turn=0; turn<arr.length-1; turn++){
+            int swap=0;
             for(int j=0; j<arr.length-1-turn; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
-                }
+                    swap++;
+                } 
             }
+            System.out.println("Swaps in turn " + (turn + 1) + ": " + swap);
+            
         }
     }
 
