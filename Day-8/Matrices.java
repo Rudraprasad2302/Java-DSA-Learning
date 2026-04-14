da0365267d7ef498e6c5a753637733270f42846c
Rudraprasad2302 <wwwrudraprasad2302@gmail.com>
2026-04-14 21:22:57 +0530

    search matrix

--- Day-8/Matrices.java
+++ Day-8/Matrices.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
 public class Matrices {
+    public static boolean search(int matrix[][], int key){
+        for(int i=0; i<matrix.length; i++){
+            for(int j =0; j<matrix[0].length; j++){
+                if(matrix[i][j] == key){
+                    System.out.println("found at cell(" + i +","+j+")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("key not found");
+        return false;
+    }
     public static void main(String[] args) {
         int matrix[][] = new int[3][3];
         int n=3, m=3;
@@ -18,6 +30,7 @@ public class Matrices {
             }
             System.out.println();
         }
+        search(matrix, 7);
     }
     
 }
